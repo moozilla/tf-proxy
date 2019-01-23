a50ac1f4df15c272a2c4c91ef21905115b9e4226
metazilla <evilgeniuscow@gmail.com>
2019-01-22 22:47:16 -0800

    Clean up after linting

--- fumen.py
+++ fumen.py
@@ -1,42 +1,46 @@
 """Parse fumen specifications.
 
-Based on reverse engineering source code from fumen v1.15a. This is only a partial implementation, will ignore mino placement and special functions (mirror, etc).
-I only need to parse the field and comments for commmunicating with solution-finder/storing my own data.
+Based on reverse engineering source code from fumen v1.15a. This is only a partial
+implementation, will ignore mino placement and special functions (mirror, etc).
+Taken from my work on setup-finder.
 
-(Todo: Look into --split option in sfinder, this might be useful later for determining pieces sequences that work for a paticular solution.
-Might require parsing pieces as well.)
 """
 
 from urllib.parse import quote
 
 FIELD_BLOCKS = 240  # number of blocks on field in fumen frame (24 rows of 10)
-ENC_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"  # used for pseudo-base64 decoding
-ASC_TABLE = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"  # for decoding comments
+# used for pseudo-base64 decoding
+ENC_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+# for decoding comments
+ASC_TABLE = (
+    " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`"
+    "abcdefghijklmnopqrstuvwxyz{|}~"
+)
+BLANK_ROW = [0] * 10
 
 
 def data_to_field(field_data):
     """Convert raw field data into matrix representation."""
     # split into rows
-    field = [field_data[i:i + 10] for i in range(0, FIELD_BLOCKS, 10)]
-    BLANK_ROW = [0] * 10
+    field = [field_data[i : i + 10] for i in range(0, FIELD_BLOCKS, 10)]
     start = next((i for i, v in enumerate(field) if v != BLANK_ROW), None)
     if start is None:
         raise ValueError("Field is blank.")
     stripped = field[start:23]  # strip blank top rows and blank bottom row
     stripped.reverse()  # TetField stores rows in reverse order
     return stripped
-    #return [[1 if b > 0 else 0 for b in row] for row in stripped] # this strips colors, should be optional
 
 
 def decode(fumen_str):
+    """Decode a fumen diagram."""
     if fumen_str[:5] != "v115@":
         raise ValueError("Unsupported fumen version.")
 
     enc_data = fumen_str[5:]
-    # need to strip ?, no clear reason why fumen even includes them (backwards compatibility maybe?)
+    # need to strip ?, no clear reason why fumen even includes them
     try:
         data = [ENC_TABLE.index(c) for c in enc_data.replace("?", "")]
-    except:
+    except ValueError:
         raise ValueError("Invalid character in fumen string.")
 
     i = 0  # data pointer
@@ -59,15 +63,20 @@ def decode(fumen_str):
     # ignoring all piece/extra data parsing here
     tmp = val // (256 * FIELD_BLOCKS)
     comment_flag = tmp % 2
-    # ignoring any sort of field copying (mirroring, rising, etc) - todo: raise exceptions
+    # todo: field copying (mirroring, rising, etc)
 
     comment = ""
     if comment_flag == 1:
         comment_len = (data[i] + (data[i + 1] * 64)) % 4096
         i += 2
         while len(comment) < comment_len:
-            val = data[i] + (data[i + 1] * 64) + (data[i + 2] * 4096) + (
-                data[i + 3] * 262144) + (data[i + 4] * 16777216)
+            val = (
+                data[i]
+                + (data[i + 1] * 64)
+                + (data[i + 2] * 4096)
+                + (data[i + 3] * 262144)
+                + (data[i + 4] * 16777216)
+            )
             i += 5
             comment += ASC_TABLE[val % 96]
             val = val // 96
@@ -78,8 +87,9 @@ def decode(fumen_str):
             comment += ASC_TABLE[val % 96]
             val = val // 96
         comment = comment[:comment_len]  # strip padding
-        # note: fumen uses unescape to support unicode, but I'm not going to bother trying to simulate unescape
-        # could potentially try urllib.parse.unquote + a regular expression to handle %uxxxx
+        # note: fumen uses unescape to support unicode, but I'm not going to bother
+        # trying to simulate unescape could potentially try urllib.parse.unquote + a
+        # regular expression to handle %uxxxx
     if len(data) - i > 0:
         raise NotImplementedError("Data remaining after first frame parsed.")
     return (data_to_field(field), comment)
@@ -87,15 +97,18 @@ def decode(fumen_str):
 
 def encode(frames):
     """Encode a fumen diagram.
-    
-    Frames is a list of tuples of (field, comment), for no comment comment should be an empty string.
-    Field is in list form, should be converted to fumen colors first.
-    Pieces and extra data stuff isn't supported. Comments must be less than 4096 characters.
-    """
 
+    Frames is a list of tuples of (field, comment), for no comment comment should be an
+    empty string.
+    Field is in list form, should be converted to fumen colors first.
+    Pieces and extra data stuff isn't supported. Comments must be less than 4096
+    characters.
+
+    """
     data = []
     prev_comment = ""
-    ct_flag = 1  # used to output ct flag ("Guideline" checkbox for colors) on the first frame only
+    # used to output ct flag ("Guideline" checkbox for colors) on the first frame only
+    ct_flag = 1
     prev_frame = [0] * FIELD_BLOCKS
 
     for field, comment in frames:
@@ -125,9 +138,10 @@ def encode(frames):
         data.append(val % 64)
         val = val // 64
         data.append(val % 64)
-        #ignore check for blank frame/field repeat here
+        # ignore check for blank frame/field repeat here
 
-        # piece/data output, only thing I implement here is comment flag + "ct" flag (Guideline colors)
+        # piece/data output
+        # only thing I implement here is comment flag + "ct" flag (Guideline colors)
         val = 1 if comment != prev_comment else 0
         val = 128 * FIELD_BLOCKS * ((val * 2) + ct_flag)
         ct_flag = 0  # should only be set on the first frame
@@ -138,7 +152,8 @@ def encode(frames):
         data.append(val % 64)
 
         if comment != prev_comment:
-            #quote similulates escape() in javascript, but output is not one-to-one (since escape is depreciated)
+            # quote similulates escape() in javascript, but output is not one-to-one
+            # (since escape is deprecated)
             comment_str = quote(comment[:4096])
             comment_len = len(comment_str)
 
@@ -177,3 +192,4 @@ def encode(frames):
         if i % 47 == 41:
             encode_str += "?"
     return encode_str
+
